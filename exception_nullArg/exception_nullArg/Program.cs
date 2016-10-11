5e9229e5711e0aa4bc7312f21830b99ed9385009
Koson Trachu <koson.trachu@gmail.com>
2016-10-11 22:14:05 +0700

    1

---
+++ exception_nullArg/exception_nullArg/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string s = "Hello World";
+        PrintArg(s);
+        string t = null;
+        PrintArg(t);
+    }
+    public static void PrintArg_old(string arg)
+    {
+        Console.WriteLine(arg);
+    }
+    public static void PrintArg(string arg)
+    {
+        try
+        {
+            if (null == arg)
+            {
+                ArgumentNullException myEX = new ArgumentNullException("arg");
+                throw myEX;
+            }
+
+        }
+        catch (ArgumentNullException e)
+        {
+            Console.WriteLine("Message : {0}", e.Message);
+        }
+
+    }
+}
+
