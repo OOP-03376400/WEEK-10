6ea52b0c426725744f7da8ec25c1dfbb4ac50eaf
Koson Trachu <koson.trachu@gmail.com>
2016-10-11 21:55:59 +0700

    1

---
+++ exception1/exception1/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int x = 10, y = 0;
+        x /= y; // Attempt to divide by zero--raises an exception
+
+    }
+}
